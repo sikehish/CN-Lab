89f0a7347385679cc73fae322031a21063ed5c94
Syed Hisham Akmal <hisham0502@gmail.com>
2024-01-09 23:49:09 +0530

    Modified 11b

--- Part B/Lab 11/11b.tcl
+++ Part B/Lab 11/11b.tcl
@@ -81,3 +81,15 @@ $ns at 0.0 "$cbr0 start"
 $ns at 0.0 "$cbr1 start"
 $ns at 10.0 "End"
 $ns run
+
+AWK Code
+|
+BEGIN{Num_of_pkts=0;} {
+if ($1 == "r" && $3 == "1" && $4 == "AGT" && $7 == "tcp") {
+Num_of_pkts = Num_of_pkts + $8;
+}
+}
+END {
+Throughput = Num_of_pkts * 8 / $2 /1000000;
+printf("\n\n\tThroughput = %fbpms\n\n\n",Throughput);
+}
