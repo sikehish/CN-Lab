2ed48ace3de00212e268bfd3a59ab8bd25c56815
Syed Hisham Akmal <hisham0502@gmail.com>
2024-01-09 12:01:51 +0530

    Modified SortFrames.java

--- Part A/Lab 2/SortFrames.java
+++ Part A/Lab 2/SortFrames.java
@@ -1,45 +1,59 @@
-// Write a program to sort frames using appropriate sorting techniques 
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Collections;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 
-public class SortFrames{
-    public static void main(String[] args){
+public class P1 {
+    public static void main(String[] args) {
+        ArrayList<int[]> frames= new ArrayList<>();
+        Random random=new Random();
+        Scanner sc=new Scanner(System.in);
 
-         ArrayList<int[]> frames= new ArrayList<>();
-         Random random = new Random();
-
-
-        System.out.print("Enter number of frames:\t");
-        Scanner sc= new Scanner(System.in);
-
-        int n=sc.nextInt();
+        System.out.println("Enter the number of frames");
+        int num=sc.nextInt();
 
         System.out.print("\n--------\n");
 
-        for(int i=0; i<n; i++ ){
-            int seqNum = random.nextInt(500);
-            int arr[]={i, seqNum};
-            frames.add(arr);
-        }
-
-        System.out.println("Before Frame Sorting: ");
-        for(int[] ele: frames){
-            System.out.println("Frame "+ele[0]+" : "+ele[1]); //OR System.out.printf("Frame: %d : %d", ele[0], ele[1]);
+        for(int i=0; i<num; i++){
+            System.out.printf("Enter data for frame %d: ",i+1);
+            int data=sc.nextInt();
+            int seqNum=random.nextInt(500);
+            int[] frame={seqNum,data};
+            frames.add(frame);
         }
         System.out.print("\n--------\n");
 
-        sortFrames(frames);
-
-        System.out.println("After Frame Sorting: ");
-        for(int[] ele: frames){
-            System.out.println("Frame "+ele[0]+" : "+ele[1]); //OR System.out.printf("Frame: %d : %d", ele[0], ele[1]);
+        System.out.println("The frames before sorting are:");
+        for(int[] frame:frames){
+            System.out.printf("%d : %d\n",frame[0], frame[1]);
         }
-        
-    }
+        System.out.print("\n--------\n");
 
-     private static void sortFrames(ArrayList<int[]> frames) {
-        Collections.sort(frames, (a, b) -> Integer.compare(a[1], b[1]));
-    }
+        Collections.sort(frames, (a,b)-> Integer.compare(a[0],b[0]));
+
+        System.out.println("The frames after sorting are:");
+        for(int[] frame:frames){
+            System.out.printf("%d : %d\n",frame[0], frame[1]);
+        }
+    }    
 }
+
+// // Output
+// Enter the number of frames
+// 3
+
+// --------
+// Enter data for frame 1: 2 
+// Enter data for frame 2: 232
+// Enter data for frame 3: 433
+
+// --------
+// The frames before sorting are:
+// 235 : 2
+// 141 : 232
+// 375 : 433
+
+// --------
+// The frames after sorting are:
+// 141 : 232
+// 235 : 2
+// 375 : 433
